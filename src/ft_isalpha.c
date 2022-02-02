2ef301df262b9665c510e2d2286c6cf688a8fa66
tmariano <thalita@DESKTOP-54M218S.localdomain>
2022-02-02 03:46:26 -0300

    adding function isalpha

---
+++ src/ft_isalpha.c
@@ -0,0 +1,26 @@
+
+/* ********************************************************************************* */
+/*                                                                  /\_____/\        */
+/*   ft_isalpha.c                                                  /  o   o  \       */
+/*                                                                ( ==  ^  == )      */
+/*   Re-code the function is alpha.                                )         (       */
+/*   These function do not need any external functions.           (           )      */
+/*                                                               ( (  )   (  ) )     */
+/*                                                              (__(__)___(__)__)    */
+/*                                                                                   */
+/* ********************************************************************************* */
+/*
+     Checks for an alphabetic character; in the standard "C" locale, it is equivalent
+	 to (isupper(c) || islower(c)).
+
+	 Checa se o parâmetro recebido é um caracter alfabético.
+*/
+
+int ft_isalpha(int c)
+{
+	if (((c >= 'A') && (c <= 'Z')) || ((c >= 'a') && (c <= 'z')))
+	{
+		return (1024);
+	}
+	return (0);
+}
