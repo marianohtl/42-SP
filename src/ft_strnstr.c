a708370fcedb2be4fed0358ec54cf9679efeed9d
marianohtl <8.litah.8@gmail.com>
2022-02-24 00:16:25 -0300

    include: unit tests strnstr

---
+++ src/ft_strnstr.c
@@ -0,0 +1,39 @@
+/* ********************************************************************************* */
+/*                                                                  /\_____/\        */
+/*   ft_strnstr.c                                                  /  o   o  \       */
+/*                                                                ( ==  ^  == )      */
+/*   Re-code the function strnstr.                                 )         (       */
+/*   These function do not need any external functions.           (           )      */
+/*                                                               ( (  )   (  ) )     */
+/*                                                              (__(__)___(__)__)    */
+/*                                                                                   */
+/* ********************************************************************************* */
+/*
+
+*/
+
+char * ft_strnstr(const char * big, const char *little,unsigned int len)
+{
+	unsigned int index;
+	unsigned int i_big;
+	unsigned int i_little;
+
+	index = 0;
+	i_big = 0;
+	i_little = 0;
+	while (index < len)
+	{
+		if(little[i_little] == '\0')
+			return((char *)&big[index]);
+		if(big[i_big] == little[i_little])
+		{
+			i_little++;
+			i_big++;
+		}else{
+			index++;
+			i_big = index;
+			i_little = 0;
+		}
+	}
+	return((char *) 0);
+}
