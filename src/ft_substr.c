1e202b2ad17a32d1e6247f99d8e1b2d70e9adb94
marianohtl <8.litah.8@gmail.com>
2022-02-24 22:25:02 -0300

    include: unit test and fix substr function

---
+++ src/ft_substr.c
@@ -0,0 +1,53 @@
+/* ********************************************************************************* */
+/*                                                                  /\_____/\        */
+/*   ft_substr.c                                                   /  o   o  \       */
+/*                                                                ( ==  ^  == )      */
+/*   Allocates (with malloc(3)) and returns a substring            )         (       */
+/*   from the string 's'. The substring begins at index           (           )      */
+/*   'start' and is of maximum size 'len'.                       ( (  )   (  ) )     */
+/*                                                              (__(__)___(__)__)    */
+/*   Parameters                                                                      */
+/*   #1 The string from which to create the substring.                               */
+/*   #2 The start indez of the substring in the string 's'.                          */
+/*   #3 The maximum length of the substring.                                         */
+/*                                                                                   */
+/*   Return Value: The substring. NULL if the allocation fails.                      */
+/*   External Functions: malloc                                                      */
+/*                                                                                   */
+/* ********************************************************************************* */
+/*
+
+*/
+#include<stdlib.h>
+
+char *ft_substr(char const *s, unsigned int start, unsigned int len)
+{
+	char * substring;
+	int size, diff, memo, index;
+	index = 0;
+	size = 0;
+	while (s[size] != '\0')
+		size++;
+
+	diff = size - start;
+
+	if(diff < len)
+	{
+		memo = diff;
+	}else{
+		memo = len;
+	}
+	substring = malloc(memo + 1);
+	if(substring == (void *)0)
+		return(substring);
+
+	while(index < memo)
+	{
+		substring[index] = s[start];
+		index++;
+		start++;
+	}
+	substring[memo] = '\0';
+	return(substring);
+}
+
---
+++ unit_tests/substr/test_substr.c
@@ -0,0 +1,31 @@
+#include "../unit_test.h"
+#include<stdio.h>
+#include <string.h>
+
+void test_substr(char const *s, unsigned int start, unsigned int len, char
+*compare)
+{
+	int i = 0;
+	unsigned int width;
+
+	char *result =  ft_substr(s, start, len);
+	width = ft_strlen(result);
+	if(ft_strncmp(result, compare, len) != 0 && result[width] != '\0')
+	{
+		printf("Return Error\n\tResult   `%s`\n\tExpected `%s`\n", result, compare);
+		return;
+	}
+
+	printf("Return Ok\n");
+}
+
+int main()
+{
+	test_substr("banana", 0, 6, "banana");
+	test_substr("banananu", 2, 8,"nananu");
+	test_substr("banananu", 2, 8,"nananu");
+	test_substr("gatos", 0, 4,"gato");
+	test_substr("", 0, 1,"");
+	test_substr("normal", 80, 3,"");
+	test_substr("normal", 1, 0,"");
+}
