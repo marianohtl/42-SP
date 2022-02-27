ecec7e38715d20e69c41c7efb11a1d4e28a9db54
marianohtl <8.litah.8@gmail.com>
2022-02-27 03:20:00 -0300

    maintenance: unit tests putstr_fd

--- unit_tests/putstr_fd/test_putstr_fd.c
+++ unit_tests/putstr_fd/test_putstr_fd.c
@@ -7,17 +7,11 @@
 void test_putstr_fd(char *s, int fd, char * compare)
 {
 	int i = 0;
-	unsigned int width;
 
+	dprintf(fd, "`");
 	ft_putstr_fd(s, fd);
-	width = ft_strlen(compare);
-	if(0==0)
-	{
-		printf("Return Error\n\tResult   `%s`\n\tExpected `%s`\n");
-		return;
-	}
+	dprintf(fd, "` == `%s`\n", compare);
 
-	printf("Return Ok\n");
 }
 
 int	main(void)
