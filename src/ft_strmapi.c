07c32a61003649450e5d87003ca53356da031bcd
marianohtl <8.litah.8@gmail.com>
2022-02-28 18:53:24 -0300

    maintenance: fix function and unit test strmapi

--- src/ft_strmapi.c
+++ src/ft_strmapi.c
@@ -1,22 +1,18 @@
+#include"libft.h"
 #include<stdlib.h>
 
-char *ft_strmapi(char const *s, char(*f)(unsigned int, char)){
-	int index;
-	char *result;
+char	*ft_strmapi(char const *s, char(*f)(unsigned int, char))
+{
+	int		index;
+	char	*result;
 
+	result = malloc((ft_strlen(s) + 1) * sizeof(*result));
 	index = 0;
-
-	while(s[index] != '\0')
-		index++;
-
-	result = malloc(index + 1);
-	index = 0;
-	while(s[index] != '\0')
+	while (s[index] != '\0')
 	{
 		result[index] = f(index, s[index]);
 		index++;
 	}
 	result[index] = '\0';
-
-	return(result);
+	return (result);
 }
