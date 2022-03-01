fda6e8cd3abc3f77842aa6f7a245110a8deca303
marianohtl <8.litah.8@gmail.com>
2022-03-01 00:33:07 -0300

    maintenance: fixes lstclear  unit test and function

--- unit_tests/lstclear/test_lstclear.c
+++ unit_tests/lstclear/test_lstclear.c
@@ -6,42 +6,65 @@
 
 void test_lstclear(t_list **lst, void (*del) (void*))
 {
-	int i = 0;
-	unsigned int width;
-
 	ft_lstclear(lst,del);
+	if (*lst != NULL)
+	{
+		printf("Return Error `%ld`",(long) lst);
+		return ;
+	}
 	printf("Return Ok\n");
 }
-
-void del_with_a(void *s)
+void    to_a_str(void *content)
 {
-	int i;
-	i = 0;
-	while (((char *)s)[i])
-	{
-		((char *)s)[i] = 'a';
-		i++;
-	}
+    char    *string;
+    int        index;
+
+	index = 0;
+    string = content;
+    while (string[index] != '\0')
+    {
+        string[index] = 'a';
+        index++;
+    }
 }
-int	main(void)
-{
-	t_list	*lst0;
-	t_list	*new0;
-	t_list	*lst1;
-	t_list	*new1;
-	t_list	*new2;
 
-	lst0 = ft_lstnew("Eu quero ver voce ");
-	new0 = ft_lstnew("me chamar de amendoim");
-	lst1 = ft_lstnew("Lagartixa");
-	new1 = ft_lstnew("tixastixa");
-	new2 = ft_lstnew("Gatinhos");
-	lst0->next = new0;
-	new0->next = lst1;
-	lst1->next = new1;
-	new1->next = new2;
-	//test_lstclear(&new2, del_with_a);
-	//test_lstclear(&lst1, del_with_a);
-	test_lstclear(&lst0, del_with_a);
-	test_lstclear(((void *) 0), del_with_a);
+void    del_str(void *content)
+{
+    char    *string;
+    int        index;
+
+    string = content;
+    while (string[index] != '\0')
+    {
+        string[index] = 0;
+        index++;
+    }
+}
+
+int    main(void)
+{
+    t_list    *lst0;
+    t_list    *new0;
+    t_list    *lst1;
+    t_list    *new1;
+    t_list    *new2;
+    t_list    *new3;
+    t_list    *lst2;
+
+    lst0 = ft_lstnew(ft_strdup("Eu quero ver voce "));
+    new0 = ft_lstnew(ft_strdup("me chamar de amendoim"));
+    lst1 = ft_lstnew(ft_strdup("Lagartixa"));
+    new1 = ft_lstnew(ft_strdup("tixastixa"));
+    new2 = ft_lstnew(ft_strdup("Gatinhos"));
+    new3 = ft_lstnew(ft_strdup("zinhos"));
+    ft_lstadd_back(&lst0, new0);
+    // ft_lstadd_back(&lst0, lst1);
+    ft_lstadd_back(&lst1, new1);
+    ft_lstadd_back(&lst1, new2);
+    ft_lstadd_back(&lst1, new3);
+	lst2 = NULL;
+	// lst1 = lst0->next->next;
+    test_lstclear(&lst1, to_a_str);
+    test_lstclear(&lst0, del_str);
+    test_lstclear(&lst2, del_str);
 }
