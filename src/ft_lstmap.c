55f290e6dad394a7537edd677cc628d85d80938d
marianohtl <8.litah.8@gmail.com>
2022-02-28 23:31:34 -0300

    maintenance: fixes in  strmap unit test and function

--- src/ft_lstmap.c
+++ src/ft_lstmap.c
@@ -27,27 +27,25 @@ Description
 Iterates the list 'lst' and applies the function 'f' to the content of each element. Creates a new list resulting of the successive applications of the function 'f'. The 'del' function is used to delete the content fo an element if needed.
 
 */
-
 #include"libft.h"
 #include<stdlib.h>
 
-t_list *ft_lstmap(t_list *lst, void *(*f)(void*),void(*del)(void*))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void*), void(*del)(void*))
 {
-	t_list *nxt_lst, *current_lst, *previous_lst, *copy_lst;
-	nxt_lst = lst;
-	previous_lst = ((void*)0);
-	while (nxt_lst->next != (void*)0)
+	t_list	*lst_map;
+	t_list	*new_lst;
+
+	lst_map = NULL;
+	while (lst != NULL)
 	{
-		current_lst = malloc(sizeof(t_list));
-		current_lst->content = f(nxt_lst->content);
-		if(previous_lst == (void*)0)
+		new_lst = ft_lstnew(f(lst->content));
+		if (new_lst == NULL)
 		{
-			copy_lst = current_lst;
-			previous_lst = current_lst;
-		}else{
-			previous_lst->next = current_lst;
+			ft_lstclear(&lst_map, del);
+			return (lst_map);
 		}
-		nxt_lst= nxt_lst->next;
+		ft_lstadd_back(&lst_map, new_lst);
+		lst = lst->next;
 	}
-	return(copy_lst);
+	return (lst_map);
 }
